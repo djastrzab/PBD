fc4eb8bf1eac9a0356f1fe25a4ded3e5af8a04f9
DHawk98 <44686772+DHawk98@users.noreply.github.com>
2020-01-23 15:58:50 +0100

    Repair and testing of procedures

--- projekt/generate_people.sql
+++ projekt/generate_people.sql
@@ -1,22 +1,12 @@
 USE [u_jastrzab]
 GO
 
-declare @i int = 0
-while @i<20
-begin
-INSERT INTO [dbo].[People]
-           ([Firstname]
-           ,[Lastname]
-           ,[Student]
-           ,[CompanyID])
-     VALUES
-           ('name'
-           ,'lastname'
-           ,1
-		   ,null
-           )
-SET @i = @i + 1;
-end
-GO
+--exec dbo.AddAddress "Sezamkowa",'43-311',Bielsko,Polska
+--exec dbo.AddCompany "PKO",999999999,"Bank Polski"
+--exec dbo.AddCompanyClient "PKO",999999999,"Bank Polski","Contact@PKO.pl",792333333,"Sezamkowa",'43-300',"Bielsko","Polska"
+--Foreign key
+--exec dbo.AddConference "£aciñskie przys³owia","M¹droœci pradawnych",0.5,100,0.01,1,"Sezamkowa","43-300","Bielsko",Polska,"2020-11-19" 
+--exec dbo.AddConferenceDay 1,"2020-11-19",50
+--exec dbo.AddContactInformation "792333333","Contact@PKO.pl",4
 
 
---
+++ projekt/remove all data.sql
@@ -0,0 +1,6 @@
+USE [u_jastrzab]
+GO
+
+EXEC sys.sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL' 
+EXEC sys.sp_msforeachtable 'DELETE FROM ?' 
+EXEC sys.sp_MSForEachTable 'ALTER TABLE ? CHECK CONSTRAINT ALL' 
